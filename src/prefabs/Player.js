ef1d30c022be330acb6eb5216d8b0817efbee79d
BrianQuintero <bquinte1@ucsc.edu>
2020-05-26 01:36:12 -0700

    Basic Player Code

--- src/prefabs/Player.js
+++ src/prefabs/Player.js
@@ -3,25 +3,43 @@ class Player extends Phaser.Physics.Arcade.Sprite{
         super(scene,x,y,texture,frame);
         scene.add.existing(this);
         scene.physics.add.existing(this);
+        this.maxBattery = 25;
+        this.currentBattery = this.maxBattery;
     }
     update(){
         //key down
         if(keyLEFT.isDown){
             this.setVelocityX(-100);
+            this.currentBattery -= .01;
         }
         else if(keyRIGHT.isDown){
             this.setVelocityX(100);
+            this.currentBattery -= .01;
         }
         else if(keyUP.isDown){
             this.setVelocityY(-100);
+            this.currentBattery -= .01;
         }
         else if(keyDOWN.isDown){
             this.setVelocityY(100);
+            this.currentBattery -= .01;
         }
         //key up
         if(keyLEFT.isUp && keyRIGHT.isUp && keyUP.isUp && keyDOWN.isUp){
             this.setVelocityX(0);
             this.setVelocityY(0);
+            this.currentBattery -= 0;
         }
+    }
+    recoverCharge(number){
+        if(this.currentBattery + number > this.maxBattery){
+            this.currentBattery = this.maxBattery;
+        }
+        else{
+            this.currentBattery += number;
+        }
+    }
+    increaseMaxCharge(number){
+        this.maxBattery += number;
     }
 }
--- src/scenes/Overworld.js
+++ src/scenes/Overworld.js
@@ -21,31 +21,37 @@ class Overworld extends Phaser.Scene{
         keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
         keyUP = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.UP);
         keyDOWN = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.DOWN);
+        keyS = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
+        keyA = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
+
         //boundary crossing and creation
         this.boundaryMinX = 0;
-        this.boundaryMaxX = 256;
+        this.boundaryMaxX = game.config.width;
         this.boundaryMinY = 0;
-        this.boundaryMaxY = 176
+        this.boundaryMaxY = game.config.height;
         this.inBounds = true;
-        //const camera = this.cameras.main;
         //player spawn
         const spawnPoint = map.findObject("Player", obj => obj.name === "PlayerSpawn");
+        //player creation and setup
         this.player = new Player(this, spawnPoint.x, spawnPoint.y, 'player');
-        this.playerCameraPositionX = this.player.x;
+        console.log(this.player);
+        this.healthIndicator = this.add.text("Player Health: " + this.player.currentBattery);
+        this.healthIndicator.setScrollFactor(0);
+        //camera stuff
+        this.playerCameraPositionX = this.player.x; //camera position creation
         this.playerCameraPositionY = this.player.y;
         this.cameraPosX = 0;
         this.cameraPosY = 0;
         this.physics.add.collider(this.player, mainLayer);
-        //camera hitbox creation
-        this.cameraBound = this.add.rectangle(this, 0,0,256,716, 0x95b64f, 50);
-        //camera setup
         this.cameras.main.setBounds(0,0,map.widthInPixels, map.heightInPixels);
-        //this.cameras.main.startFollow(this.player);
         this.cameras.main.roundPixels = true;
-        this.add.rectangle(0,0,256,176,0xFFFFFF, 0.05).setOrigin(0,0);
     }
     update(){
         //camera stuff (I can probably shorten this some other time)
+        if(this.player.currentBattery < 1){
+            this.scene.restart();
+        }
+        this.healthIndicator.text = "Player Health: " + Math.trunc(this.player.currentBattery);
         this.playerCameraPositionX = this.player.x;
         this.playerCameraPositionY = this.player.y;
         this.player.update();
@@ -71,37 +77,38 @@ class Overworld extends Phaser.Scene{
         switch(direction){
             case "right":
                 this.player.x += 10;
-                this.boundaryMaxX += 256;
-                this.boundaryMinX += 256;
-                this.cameraPosX += 256;
+                this.boundaryMaxX += game.config.width;
+                this.boundaryMinX += game.config.width;
+                this.cameraPosX += game.config.width;
                 this.cameras.main.setScroll(this.cameraPosX,this.cameraPosY);
                 this.inBounds = true;
                 break;
             case "down":
                 this.player.y += 10;
-                this.boundaryMaxY += 176;
-                this.boundaryMinY +=  176;
-                this.cameraPosY += 176;
+                this.boundaryMaxY += game.config.height;
+                this.boundaryMinY +=  game.config.height;
+                this.cameraPosY += game.config.height;
                 this.cameras.main.setScroll(this.cameraPosX,this.cameraPosY);
                 this.inBounds = true;
                 break;
             case "left":
                 this.player.x -= 10;
-                this.boundaryMaxX -= 256;
-                this.boundaryMinX -= 256;
-                this.cameraPosX -= 256;
+                this.boundaryMaxX -= game.config.width;
+                this.boundaryMinX -= game.config.width;
+                this.cameraPosX -= game.config.width;
                 this.cameras.main.setScroll(this.cameraPosX,this.cameraPosY);
                 this.inBounds = true;
                 break;
             case "up":
                 this.player.y -= 10;
-                this.boundaryMaxY -= 176;
-                this.boundaryMinY -= 176;
-                this.cameraPosY -= 176;
+                this.boundaryMaxY -= game.config.height;
+                this.boundaryMinY -= game.config.height;
+                this.cameraPosY -= game.config.height;
                 this.cameras.main.setScroll(this.cameraPosX,this.cameraPosY);
                 this.inBounds = true;
                 break;
         }
 
     }
+
 }
